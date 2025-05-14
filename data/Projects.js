b26fe9d99f12a81216dddf746ceff73bf0787d2a
メイチャ <alichaicha248@gmail.com>
2025-05-14 21:38:35 +0700

    Add files via upload

--- data/Projects.js
+++ data/Projects.js
@@ -1,62 +1,22 @@
 export const Projects = [
   {
     id: 1,
-    name: 'Space Game Community ID',
-    slug: 'sgcid',
-    img: 'sgcid',
-    demo: '#',
-    sourceCode: 'https://github.com/euxzy/sgcid',
-    description:
-      'SGCID adalah .',
+    slug: 'makan-bareng',
+    img: 'makan-bareng',
   },
   {
     id: 2,
-    name: 'Ceritain',
-    slug: 'ceritain',
-    img: 'ceritain',
-    demo: 'https://ceritain.vercel.app',
-    sourceCode: 'https://github.com/euxzy/ceritain',
-    description:
-      'Ceritain adalah sebuah website dimana kita bisa menceritakan semua hal yang ingin kita ceritakan. (Project ini masih dalam tahap development namun sudah bisa dipakai)',
+    slug: 'foto-bersama',
+    img: 'foto-bersama',
   },
   {
     id: 3,
-    name: 'API Ceritain',
-    slug: 'api-ceritain',
-    img: 'api-ceritain',
-    demo: '#',
-    sourceCode: 'https://github.com/euxzy/api-ceritain',
-    description:
-      'API Ceritain adalah API yang digunakan di website Ceritain.',
+    slug: 'berkumpul-bareng',
+    img: 'berkumpul-bareng',
   },
   {
     id: 4,
-    name: 'Weather App',
-    slug: 'weather-app',
-    img: 'weather-app',
-    demo: '#',
-    sourceCode: 'https://github.com/euxzy/weather-app',
-    description:
-      'Weather App adalah sebuah website untuk melihat berapa suhu pada kota tertentu.',
-  },
-  {
-    id: 5,
-    name: 'Bank Metrics',
-    slug: 'metrics',
-    img: 'metrics',
-    demo: 'https://metrics-fe.fly.dev',
-    sourceCode: 'https://github.com/Strayneko/Bank-Metrics-FrontEnd',
-    description:
-      'Bank Metrics adalah sebuah aplikasi yang bisa digunakan untuk mengecek apakah pengajuan pinjaman kita disetujui atau tidak oleh bank yang terdaftar di aplikasi ini.',
-  },
-  {
-    id: 6,
-    name: 'Coming Soon Project',
-    slug: 'coming-soon-project',
-    img: 'coming-soon',
-    demo: '#',
-    sourceCode: '#',
-    description:
-      'Nantikan project-project mendatang lainnya.',
+    slug: 'welcome',
+    img: 'welcome',
   },
 ];
